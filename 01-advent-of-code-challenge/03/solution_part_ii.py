0660a2a5ac5534c6eec90b45e5ca87c86a4c5618
LeoMoraes1644 <105466685+LeoMoraes1644@users.noreply.github.com>
2023-02-14 15:54:45 +0000

    3º day finalizado.

--- 01-advent-of-code-challenge/03/solution_part_ii.py
+++ 01-advent-of-code-challenge/03/solution_part_ii.py
@@ -8,20 +8,17 @@ FILE_PATH = input('insira o path do arquivo: ')
 with open(FILE_PATH) as file:
     rucksacks = [i for i in file.read().strip().split("\n")]
 
-contador = 0
-val_max = len(rucksacks)
-for index, rucksack in enumerate(rucksacks):
-    total_size = len(rucksack)
-    armazenar_letra = ""
-    for letra_um in list(rucksack):
-        if (index+1) > val_max or (index+2) > val_max:
-            break
-        for letra_dois in list(rucksacks[index+1]):
+CONTADOR = 0
+val_max = range(0, len(rucksacks), 3)
+for i in val_max:
+    ARMAZENAR_LETRA = ""
+    for letra_um in list(rucksacks[i]):
+        for letra_dois in list(rucksacks[i+1]):
             if letra_um == letra_dois:
-                for letra_tres in list(rucksacks[index+2]):
+                for letra_tres in list(rucksacks[i+2]):
                     if letra_tres == letra_um:
-                        armazenar_letra = letra_um
+                        ARMAZENAR_LETRA = letra_um
                         break
-    if len(armazenar_letra) > 0:
-        contador += convert_letter_in_value(armazenar_letra)
-print(contador)
+    if len(ARMAZENAR_LETRA) > 0:
+        CONTADOR += convert_letter_in_value(ARMAZENAR_LETRA)
+print(CONTADOR)
